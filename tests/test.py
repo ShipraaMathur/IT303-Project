4ca7f1e44282ddb7b55157e81b13af28f59c77a3
pratik-shivnani <pratikshivnani@gmail.com>
2021-09-29 20:42:51 +0550

    Initial commit

---
+++ tests/test.py
@@ -0,0 +1,45 @@
+
+class scs:
+
+    def ATC(self):
+        pass
+
+    def Airplane(self):
+        pass
+
+    def Pilot(self):
+        pass
+
+    def Runway(self):
+        pass
+
+    def assignRunway(self):
+        pass
+
+    def getLocation(self):
+        pass
+
+    def getPilot(self):
+        pass
+
+    def makeConflictAmend(self):
+        pass
+
+    def predictConflict(self):
+        pass
+
+    def resolveConflict(self):
+        pass
+
+    def returnATCfromAir(self):
+        pass
+
+    def returnATCfromPilot(self):
+        pass
+
+    def returnATCfromRunway(self):
+        pass
+
+    def runwayAssignment(self):
+        pass
+
